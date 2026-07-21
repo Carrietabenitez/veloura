af764911ce56abe4260e7b7aacddfae1d9bbf3da
Cristin Arrieta5 <cristiandres2015@gmail.com>
2026-07-21 13:55:28 -0500

    Enviar PageView a Google Analytics

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -3,9 +3,16 @@ import ReactGA from "react-ga4";
 export const initGA = () => {
   const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
-  console.log("GA ID:", measurementId);
-
   if (measurementId) {
     ReactGA.initialize(measurementId);
+
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname + window.location.search,
+      title: document.title,
+    });
+
+    console.log("Google Analytics inicializado");
+    console.log("PageView enviada");
   }
 };
