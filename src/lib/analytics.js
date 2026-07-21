cf3f27e35e8f590fc7d3c55a696f0902c72ff2b4
Cristin Arrieta5 <cristiandres2015@gmail.com>
2026-07-21 13:51:10 -0500

    Configurar Google Analytics

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -3,11 +3,12 @@ import ReactGA from "react-ga4";
 export const initGA = () => {
   const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
+  console.log("GA ID:", measurementId);
+
   if (measurementId) {
     ReactGA.initialize(measurementId);
-    ReactGA.send({
-      hitType: "pageview",
-      page: window.location.pathname,
-    });
+    console.log("Analytics inicializado");
+  } else {
+    console.log("No se encontró el Measurement ID");
   }
 };
